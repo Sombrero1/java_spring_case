17430bb51b8e2b938567fe58846c8cc51f57fb35
sombrero <vovashevchenko1964@mail.ru>
2020-11-26 21:58:23 +0300

    ещё аннотации

--- java_spring_start/src/main/java/ru/mirea/spring/MusicPlayer.java
+++ java_spring_start/src/main/java/ru/mirea/spring/MusicPlayer.java
@@ -3,8 +3,14 @@ package ru.mirea.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ru.mirea.spring.Music.Music;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class MusicPlayer {
@@ -14,6 +20,10 @@ public class MusicPlayer {
     @Autowired
     @Qualifier("rockMusic")
     Music music2;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int value;
 
     enum Ch{
         CLASSICAL, ROCK;
@@ -23,7 +33,12 @@ public class MusicPlayer {
         else System.out.println(music2.getMusic());
 
     }
+    @PreDestroy
+    @PostConstruct
     private void initdo(){
+        System.out.println(name + " : " + value);
 
     }
+
+
 }
