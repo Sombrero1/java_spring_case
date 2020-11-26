f1ec482c1c19c11069e4950acc47c34e82b07400
sombrero <vovashevchenko1964@mail.ru>
2020-11-26 22:06:15 +0300

    xml -> java

---
+++ java_spring_start/src/main/java/ru/mirea/spring/SpringConfig.java
@@ -0,0 +1,13 @@
+package ru.mirea.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.mirea.spring")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+
+
+}
